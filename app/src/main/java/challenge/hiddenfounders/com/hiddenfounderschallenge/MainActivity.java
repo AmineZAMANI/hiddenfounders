47f20bb5566077b77169fdaba687f8ff7bbc099f
CORP\azamani <amine.zamani@capgemini.com>
2017-12-27 15:20:01 +0000

    * Implementing generic adapters (Pending) * Implementing Google gson parse library api (OK) * Implementing generic async task (OK)

--- app/src/main/java/challenge/hiddenfounders/com/hiddenfounderschallenge/MainActivity.java
+++ app/src/main/java/challenge/hiddenfounders/com/hiddenfounderschallenge/MainActivity.java
@@ -3,11 +3,21 @@ package challenge.hiddenfounders.com.hiddenfounderschallenge;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import challenge.hiddenfounders.com.hiddenfounderschallenge.beans.Callback;
+import challenge.hiddenfounders.com.hiddenfounderschallenge.beans.GenericAsyncTask;
+import challenge.hiddenfounders.com.hiddenfounderschallenge.utils.JsonUtils;
+
+public class MainActivity extends AppCompatActivity implements Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new GenericAsyncTask(this).execute("https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&order=desc");
+    }
+
+    @Override
+    public void onResult(String result) {
+        System.out.print(JsonUtils.parse(result).get("items"));
     }
 }
